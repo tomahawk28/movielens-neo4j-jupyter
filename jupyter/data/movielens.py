10c822d9b27591075a80ea25a64006b1f33d1edf
Jihyuk Bok <jhbok@newstore.com>
2018-10-25 12:49:32 +0200

    update storytelling, and index

--- jupyter/data/movielens.py
+++ jupyter/data/movielens.py
@@ -2,6 +2,9 @@ from py2neo import Graph
 class MovieLens():
     def __init__(self, host):
         self.graph = Graph(f"bolt://{host}")
+        # create indexes
+        assert self.graph.run("CREATE INDEX ON :Movie(movieId)").data() == []
+        assert self.graph.run("CREATE INDEX ON :Movie(title)").data() == []
 
     def find_movies_by_name(self, name, case_insensitive=True, limit=10):
         return self.graph.run(f"""MATCH (movie:Movie)
@@ -15,13 +18,14 @@ class MovieLens():
             RETURN m.movieId as movieId, m.title as title, 
             avg(r.rating) as average_rate, count(r) as count""").data()
 
-    def recommendation(self, movieIds, rating=3.5, count=4, limit=5):
-        return self.graph.run(f"""MATCH (m:Movie)-[r1]-(u)-[r]-(m2)
+    def recommendation(self, movieIds, category=None, rating=3.5, count=4, limit=5):
+        return self.graph.run(f"""MATCH (m:Movie)-[r1]-(u)-[r]-(m2{':`' + category + '`' if category else ''})
             WHERE m.movieId IN {movieIds} AND r1.rating > {rating}
             WITH count(*) as cnt, avg(r.rating) as rate, m2
             WHERE rate > {rating} AND cnt > {count}
             RETURN DISTINCT m2.movieId as movieId,
             m2.title as title, cnt as count, rate as average_rate
-            ORDER BY cnt DESC, rate DESC LIMIT 10""").data()
+            ORDER BY cnt DESC, rate DESC LIMIT {limit}""").data()
+
 
 
